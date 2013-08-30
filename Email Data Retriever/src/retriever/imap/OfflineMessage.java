f5058a64021e17cad3464c8910ef33b469f4d4dc
Jacob Bartel <bartel@cs.unc.edu>
2013-08-30 16:32:36 -0400

    Added more stored values in Offline Message

--- Email Data Retriever/src/retriever/imap/OfflineMessage.java
+++ Email Data Retriever/src/retriever/imap/OfflineMessage.java
@@ -1,8 +1,10 @@
 package retriever.imap;
 
+import java.util.Date;
 import java.util.Map;
 import java.util.TreeMap;
 
+import javax.mail.Address;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
@@ -10,6 +12,10 @@ public class OfflineMessage extends MimeMessage {
 
 	MimeMessage parent;
 	private final Map<String, String[]> seenHeaders = new TreeMap<>();
+	private String subject = null;
+	private Date receivedDate;
+	private Address[] from;
+	private Address[] allRecipients;
 
 	public OfflineMessage(MimeMessage parent) throws MessagingException {
 		super(parent);
@@ -27,5 +33,37 @@ public class OfflineMessage extends MimeMessage {
 		} else {
 			return seenHeaders.get(header);
 		}
+	}
+
+	@Override
+	public String getSubject() throws MessagingException {
+		if (subject == null) {
+			subject = parent.getSubject();
+		}
+		return subject;
+	}
+
+	@Override
+	public Date getReceivedDate() throws MessagingException {
+		if (receivedDate == null) {
+			receivedDate = parent.getReceivedDate();
+		}
+		return receivedDate;
+	}
+
+	@Override
+	public Address[] getFrom() throws MessagingException {
+		if (from == null) {
+			from = parent.getFrom();
+		}
+		return from;
+	}
+
+	@Override
+	public Address[] getAllRecipients() throws MessagingException {
+		if (allRecipients == null) {
+			allRecipients = parent.getAllRecipients();
+		}
+		return allRecipients;
 	}
 }
