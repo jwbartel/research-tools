ca1cb1bbe8f47fb9c2c19309bfd6bb6e1972b3c7
Jacob Bartel <bartel@cs.unc.edu>
2013-09-18 15:10:25 -0400

    Added a loading popup when the user submits their credentials

--- Email Data Retriever/web/retriever.js
+++ Email Data Retriever/web/retriever.js
@@ -17,5 +17,12 @@ function updateDepthCounts() {
 }
 
 function showLoading() {
-	document.getElementById('overlay').style.visibility = 'visible';
+	divElement = document.createElement('div');
+	divElement.innerHTML = "<div id='overlay'></div>" +
+		"<div id='loadingContainer'><div id='loadingMessage'>" +
+		"<p> Thank you for contributing.  Your anonymized data is being retrieved. You will be redirected to our reviewing tool after retrieval completes.</p>" +
+		"<center><img src='img/spinner.gif' alt='Loading...'></img></center>" +
+		"<p> <a href=''>If you would rather not wait, please click here to be emailed when your data finishes processing.</a></p>" +
+		"</div></div>";
+	document.body.appendChild(divElement);
 }
