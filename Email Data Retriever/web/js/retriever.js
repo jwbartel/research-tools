7bf2ec77c06d5bd85779c55d06b36350f292c769
Jacob Bartel <bartel@cs.unc.edu>
2013-09-19 13:47:03 -0400

    Fixed more javascript to match jquery

--- Email Data Retriever/web/js/retriever.js
+++ Email Data Retriever/web/js/retriever.js
@@ -12,12 +12,15 @@ function switchFileNamesEnabled() {
 }
 
 function showLoading() {
-	divElement = document.createElement('div');
-	divElement.innerHTML = "<div id='overlay'></div>" +
-		"<div id='loadingContainer'><div id='loadingMessage'>" +
-		"<p> Thank you for contributing.  Your anonymized data is being retrieved. You will be redirected to our reviewing tool after retrieval completes.</p>" +
-		"<center><img src='img/spinner.gif' alt='Loading...'></img></center>" +
-		"<p> <a href=''>If you would rather not wait, please click here to be emailed when your data finishes processing.</a></p>" +
-		"</div></div>";
-	document.body.appendChild(divElement);
+	$('<div></div>').prop('id','overlay')).appendTo('body');
+	$('<div></div>').prop('id', 'loadingContainer').appendTo('body');
+			.append($('<p></p>')
+					.append('Thank you for contributing.  ')
+					.append('Your anonymized data is being retrieved. ')
+					.append('You will be redirected to our reviewing tool after retrieval completes.'))
+			.append($('<center></center>').append($('<img></img>')
+					.prop('src', 'img/spinner.gif')
+					.prop('alt', 'Loading...'))
+			.append($('<p></p>').append($('<a></a>')
+					.append('If you would rather not wait, please click here to be emailed when your data finishes processing.')))))
 }
