ae7a3c3189375e5e8783f2fe1f5f7f2ae6b3f4e4
Jacob Bartel <bartel@cs.unc.edu>
2013-09-20 12:25:51 -0400

    Created jar that runs from web app

---
+++ Email Data Retriever/src/retriever/view/commandline/CommandLineEmailDataRetriever.java
@@ -0,0 +1,14 @@
+package retriever.view.commandline;
+
+/**
+ * Tool to collect data from messages from the command line
+ * 
+ * @author bartel
+ * 
+ */
+public class CommandLineEmailDataRetriever {
+
+	public static void main(String[] args) {
+		System.out.println("Data Retriever called");
+	}
+}
--- Email Data Retriever/web/php/retriever.php
+++ Email Data Retriever/web/php/retriever.php
@@ -1,1 +1,5 @@
 Returning this will indicate that the retrieval process has completed.  It should also redirect to a UI to view the submitted data.
+
+<?php 
+	print exec('java -jar EmailDataRetriever.jar');
+?>
