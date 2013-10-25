4a04d314b61e30c39c959ca705619c1faa845670
Jacob Bartel <bartel@cs.unc.edu>
2013-10-25 18:31:26 -0400

    Added collection of calendar data for a limited time frame and for a limited set of features

--- Email Data Retriever/web/php/google-auth/calendarRetriever.php
+++ Email Data Retriever/web/php/google-auth/calendarRetriever.php
@@ -14,6 +14,58 @@ $client->setClientSecret($apiConfig['oauth2_client_secret']);
 $client->setRedirectUri($apiConfig['oauth2_redirect_uri']);
 $service = new Google_CalendarService($client);
 
+function eventStrList($events_list) {
+	$events_str_list = array();
+	foreach ($events_list as $event) {
+		$str_val = '';
+		if (isset($event['visibility'])) {
+			$str_val .= 'visibility: '.$event['visibility']."\n";
+		} else {
+			$str_val .= 'visibility: default'."\n";
+		}
+		$str_val .= 'summary: '.$event['summary']."\n";
+		$str_val .= 'status: '.$event['status']."\n";
+		$str_val .= 'created: '.$event['created']."\n";
+		$str_val .= 'updated: '.$event['updated']."\n";
+		if (isset($event['creator']) && isset($event['creator']['email'])) {
+			$str_val .= 'creator: '.$event['creator']['email']."\n";
+		}
+		if (isset($event['creator']) && isset($event['creator']['self'])) {
+			$str_val .= 'creator-is-self: '.$event['creator']['self']."\n";
+		}
+		if (isset($event['organizer']) && isset($event['organizer']['email'])) {
+			$str_val .= 'organizer: '.$event['organizer']['email']."\n";
+		}
+		if (isset($event['organizer']) && isset($event['organizer']['self'])) {
+			$str_val .= 'organizer-is-self: '.$event['organizer']['self']."\n";
+		}
+		$str_val .= 'start: '.$event['start']['dateTime']."\n";
+		$str_val .= 'end: '.$event['end']['dateTime']."\n";
+		if (isset($event['endTimeUnspecified'])) {
+			$str_val .= 'endTimeUnspecified: '.$event['endTimeUnspecified']."\n";
+		}
+
+		$str_val .= 'attendees:'."\n";
+		foreach ($event['attendees'] as $attendee) {
+			if (isset($attendee['email']) || isset($attendee['responseStatus'])) {
+				if (isset($attendee['email'])) {
+					$str_val .= "\temail: ".$attendee['email']."\n";
+				}
+				if (isset($attendee['responseStatus'])) {
+					$str_val .= "\tresponseStatus: ".$attendee['responseStatus']."\n";
+				}
+				if (isset($attendee['self'])) {
+					$str_val .= "\tself: ".$attendee['self']."\n";
+				}
+				$str_val .= "\t\n";
+			}
+		}
+		
+		array_push($events_str_list, $str_val);
+	}
+	return $events_str_list;
+}
+
 if (isset($_GET['code'])) {
 	$client->authenticate();
 	$_SESSION['token'] = $client->getAccessToken();
@@ -33,24 +85,50 @@ if (!($client->getAccessToken())){
 }else {
 	//header('Location: https://wwwx.cs.unc.edu/~bartel/cgi-bin/emailsampler') ;
 	
-	$maxEvents = $_GET['events'];
-	if ( is_null($maxEvents) || strcmp($maxEvents,"") == 0) {
-		$maxEvents = 50;
+	$maxMonths = 12;
+	if (isset($_GET['maxMonths'])) {
+		$maxMonths = $_GET['maxMonths'];
 	}
+	
 	$optParams = array(
 		'timeZone' => 'America/New_York',
+		'timeMin' => date("Y-m-d\TH:i:sP", strtotime('-'.$maxMonths.' months')),
  		'timeMax'=>  date("Y-m-d\TH:i:sP"),
-// 		'timeMin'=>  date("Y-m-d\TH:i:sP",0),
-		'singleEvents' => true,
+ 		'singleEvents' => true,
 	);
+	print 'retrieving events....';
 	$events = $service->events->listEvents('primary', $optParams);
 	$events_list = array_reverse($events['items']);
-	print 'Your Events: <pre>' . print_r($events, true) . '</pre>';
+	$prev_events_list = array();
+	print 'done<br>';
+	$events_count = 0;
+	while (array_key_exists('nextPageToken', $events)) {
+		print 'retrieving events....';
+		array_push($prev_events_list, $events_list);
+		$optparams['pageToken'] = $events['nextPageToken'];
+		$events = $service->events->listEvents('primary', $optParams);
+		$events_list = array_reverse($events['items']);
+		print 'done<br>';
+		$count++;
+		if ($count > 3){
+			break;
+		}
+	}
 	
-// 	for( $i=0; $i<max_events; $i++) {
-// 		print print_r($events_list[$i],true);
-// 		print '\n';
-// 	}
+ 	if (count($prev_events_list) > 0) {
+		$prevEventsPos = count($prev_events_list)-1;
+		$pos = 0;
+		while ($prevEventsPos >= 0) {
+			array_push($events_list, $prev_events_list[$prevEventsPos][$pos]);
+			$pos++;
+			if ($pos >= count($prev_events_list[$prevEventsPos])) {
+				$pos = 0;
+				$prevEventsPos --;
+			}
+		}
+	}
+	//print 'Your Events: <pre>' . print_r($events_list, true) . '</pre>';
+	print 'Your Events: <pre>' . print_r(eventStrList($events_list), true) . '</pre>';
 
 	// We're not done yet. Remember to update the cached access token.
 	// Remember to replace $_SESSION with a real database or memcached.
