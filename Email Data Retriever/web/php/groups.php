4c244e60e59929434937e9e1a9556281a7f617a2
Andrew Ghobrial <awghobrial@gmail.com>
2014-07-25 18:03:27 -0400

    added groups feedback page

--- Email Data Retriever/web/php/groups.php
+++ Email Data Retriever/web/php/groups.php
@@ -1,25 +1,4 @@
-<!DOCTYPE html>
-    <html>
-<head>
-    <title>E-mail Thread Data Groups</title>
-    <link rel="stylesheet" href="groups.css">
-    <script>
-        function addremove(id) {
-            if(document.getElementById(id).getAttribute("style")==="background-color:red") {
-                document.getElementById(id).setAttribute("style", "background-color:lightgreen");
-                // insert code to add member to group
-            }
-            else {
-                document.getElementById(id).setAttribute("style", "background-color:red");
-                // insert code to remove member from group
-            }
-        }
-
-    </script>
-</head>
-<body>
-    <div class="center" id="reviewer">
-        <?php
+<?php
         $record_id = $_GET['r'];
         $private_folder= '/afs/cs.unc.edu/home/bartel/email_threads/private_data/'.$record_id;
 
@@ -39,37 +18,165 @@
             fclose($file_handle);
         }
         ?>
-        <table style='width:100%'>
+<!DOCTYPE html>
+    <html>
+<head>
+    <title>E-mail Thread Data Groups</title>
+    <link rel="stylesheet" href="groups.css">
+    <link rel="stylesheet" href="../js/jquery-ui-1.11.0.custom/jquery-ui.min.css">
+    <script src='../js/jquery-1.10.2.min.js' type='text/javascript'></script>
+    <script>
+    var record_id = '<?php print $record_id; ?>';
+    var changes = new Array();
+
+        function addremove(id) {
+            if(document.getElementById(id).getAttribute("style")==="background-color:red") {
+                document.getElementById(id).setAttribute("style", "background-color:white");
+                // insert code to add member to group
+                changes.push("Group "+ $('#'+id).parent().parent().attr('id') + ": Added: "+ $('#'+id+'-email').text());
+                console.log(changes);
+                $('#'+id+'-close').html("Remove");
+            }
+            else {
+                document.getElementById(id).setAttribute("style", "background-color:red");
+                // insert code to remove member from group
+                changes.push("Group "+ $('#'+id).parent().parent().attr('id') + ": Removed: "+ $('#'+id+'-email').text());
+                console.log(changes);
+                $('#'+id+'-close').html("Undo");
+                $('#'+id).remove();
+            }
+        }
+        function deletegroup(id) {
+            changes.push("Group " + id + ": Delete Group");
+            console.log(changes);
+            $('#'+id).remove();
+        }
+        function submitData() {
+            $.ajax({
+                type: "POST",
+                url: "update_groups.php",
+                data: {r:record_id, edits:changes},
+                success: function(data, textStatus, jqXHR) {
+                    console.log(textStatus);
+                    window.location.href = "https://docs.google.com/forms/d/1jgkrkOdFAs9vEVmPYjTI-kbjbS-8ZIhFC9ORBacLoX0/viewform?usp=send_form";
+}
+                
+        })
+}
+        var otherId = -1;
+        function addOther(id) {
+            var otherAddress = $('#'+id+'-other').val();
+            changes.push("Group " + id + ": Added: " + otherAddress);
+            console.log(changes);
+            otherId--;
+
+            $('#'+id+'-buttons').append('<div class="button" id="'+otherId+'"> <div class="email_address" id="'+otherId+'-email"> '+otherAddress+'</div> <div class="close" id="'+otherId+'-close" onClick="addremove(\''+otherId+'\')">Remove</div></div> ');
+        }
+        var customIdNumber = 1;
+        function addGroup(name) {
+            var customId = 'custom-'+customIdNumber;
+            var newGroupCode = '';
+            newGroupCode += '<div id="'+customId+'">';
+            newGroupCode += '<b>Group Name: </b> <input type="text" value="'+name+'" id="'+customId+'-name"><br>';
+            newGroupCode += '<button type="button" onClick="deletegroup(\''+customId+'\')">Delete Group</button><br>';
+            newGroupCode += '<div id="'+customId+'-buttons">';      
+            newGroupCode += '</div>';
+            newGroupCode += '<br> Other: <input class="otherAddress" type="text" id="'+customId+'-other"/>';
+            newGroupCode += '<button type="button" onClick="addOther(\''+customId+'\')">Add</button>';
+            newGroupCode += '</div>';
+            newGroupCode += '<br><br><br>';
+
+            $('#allGroups').append(newGroupCode);
+            $('#'+customId+'-name').on('input', function() {
+                changes.push('Group '+ customId+' Name Typed: ' +$('#'+customId+'-name').val());
+                console.log(changes);
+            });
+            $('#'+customId+'-other').on('input', function() {
+                changes.push('Group '+ customId+' Other Email Typed: ' +$('#'+customId+'-other').val());
+                console.log(changes);
+             });
+            customIdNumber++;
+        }
+    </script>
+</head>
+<body>
+    <div class="center" id="reviewer">
+       
             <?php
             writeData($groups_exist, $groups_data, 'Predicted Groups', '100%', false);
             function writeData($exists, $data, $label, $width) {
                 if ($exists) {
-                    print('<td style="width:'.$width.'">');
                     print('<h3>'.$label.'</h3>');
-                    print '<form>';
-                    foreach($data as $group) {
-                        print '<b>Group Name: </b> <input type="text" ><br>'."\n";
+                    print '<form id="allGroups">';
+                    foreach($data as $key=>$group) {
+                        print '<div id="'.($key+1).'">';
+                        print '<b>Group Name: </b> <input type="text" id="'.($key+1).'-name"><br>'."\n";
+                        print "<script> $('#".($key+1)."-name').on('input', function() {
+            changes.push('Group '+ '".($key+1)."'+' Name Typed: ' +$('#".($key+1)."-name').val());
+            console.log(changes);
+        });  </script>";
+                        print '<button type="button" onClick="deletegroup(\''.($key+1).'\')">Delete Group</button><br>';
+                        print '<div id="'.($key+1).'-buttons">';
                         foreach($group as $entry)
                             writeButton($entry);
-                        print('<br><br>'."\n");
+                        print '</div>';
+                        print '<br> Other: <input class="otherAddress" type="text" id="'.($key+1).'-other"/>';
+                        print "<script> $('#".($key+1)."-other').on('input', function() {
+            changes.push('Group '+ '".($key+1)."'+' Other Email Typed: ' +$('#".($key+1)."-other').val());
+            console.log(changes);
+        });  </script>";
+                        print '<button type="button" onClick="addOther(\''.($key+1).'\')">Add</button>';
+                        print('<br><br><br>'."\n");
+                        print '</div>';
                     }
-                    print '<button type="submit" formaction="update_groups.php">Submit and Continue</button>';
-                    print '</form>';
-                    print('</td>');
+                        print '</form>';
+                    ?>
+                        <h3> Add Custom Group</h3>
+                        <div id="AddGroup">
+                           <b>Group Name: </b> <input type="text" id="custom-name"/><br>
+                            <button type="button" id="addGroupButton" onClick="addGroup($('#custom-name').val())">Add Group</button>
+                            <script> 
+                                $('#custom-name').on('input', function() {
+                                   changes.push('Custom Group Name Typed: ' + $('#custom-name').val());
+                                   console.log(changes);
+                                  }); 
+                            </script>
+                         </div><br>
+                    <?php
+                    print '<button type="button" style="width:300px" onClick="submitData()">Submit and Continue</button>';
+                    
+                   
                 }
             }
             ?>
-        </table>
+       
         <br>
         <?php
         function writeButton ($email_address) {
             $id = uniqid();
             print "\t".'<div class = "button" id="'.$id.'">';
-                    print "\t\t".$email_address;
-                    print "\t\t".'<div class="close" onClick="addremove(\''.$id.'\')">+/-</div>'."\n";
+                    print '<div id="'.$id.'-email" class="email_address">'.$email_address.'</div>';
+                    print " ".'<div class="close" id="'.$id.'-close"  onClick="addremove(\''.$id.'\')">Remove</div>'."\n";
             print "\t".'</div>'."\n";
+            print '<script>$("#'.$id.'").css("height",$("#'.$id.'-email").height())</script>';
         }
         ?>
+        
     </div>
 </body>
     </html>
+    <script src='../js/jquery-ui-1.11.0.custom/jquery-ui.min.js' type='text/javascript'></script>
+    <script>
+    var allEmails = [
+    <?php foreach($groups_data as $key=>$group) {
+            foreach($group as $entry) {
+                print '"'.$entry.'",';
+            }
+        }
+        ?>
+        ""];
+    $(function() {
+    $( ".otherAddress" ).autocomplete({
+      source: allEmails
+    });
+  });</script>
